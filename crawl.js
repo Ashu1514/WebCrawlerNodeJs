519cfa0a2660b5451bab7cea3f5ba2cfc8f9b176
Ashu1514 <ashulondhe50@gmail.com>
2023-04-12 15:26:24 +0530

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,12 +1,40 @@
-function normalizeURL(urlString) {
-    const urlObj = new URL(urlString);
-    const hostPath =  (`${urlObj.hostname}${urlObj.pathname}`);
-    if(hostPath.length > 0 && hostPath.slice(-1) === '/'){
-        return hostPath.slice(0, -1);
+const { JSDOM } = require("jsdom");
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkEls = dom.window.document.querySelectorAll('a');
+  for(const el of linkEls){
+    if(el.href.slice(0, 1) === '/'){
+        // relative
+        try {
+            const urlObj = new URL(`${baseURL}${el.href}`);
+            urls.push(urlObj.href);
+        } catch (error) {
+            console.log(`Error with relative url ${error.message}`);
+        }
+    } else {
+        // absolute
+        try {
+            const urlObj = new URL(`${el.href}`);
+            urls.push(urlObj.href);
+        } catch (error) {
+            console.log(`Error with absolute url ${error.message}`);
+        }
     }
-    return hostPath;
+  }
+  return urls;
+}
+
+function normalizeURL(urlString) {
+  const urlObj = new URL(urlString);
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1);
+  }
+  return hostPath;
 }
 
 module.exports = {
-    normalizeURL
-}
+  normalizeURL,
+  getURLsFromHTML,
+};
--- crawl.test.js
+++ crawl.test.js
@@ -1,31 +1,100 @@
-const { normalizeURL } = require("./crawl");
+const { normalizeURL, getURLsFromHTML } = require("./crawl");
 const { test, expect } = require("@jest/globals");
 
-test('normalizeURL strip protocol', () => {
-    const input = 'https://www.linkedin.com/feed';
-    const actual = normalizeURL(input);
-    const expected = 'www.linkedin.com/feed';
-    expect(actual).toEqual(expected);
+test("normalizeURL strip protocol", () => {
+  const input = "https://www.linkedin.com/feed";
+  const actual = normalizeURL(input);
+  const expected = "www.linkedin.com/feed";
+  expect(actual).toEqual(expected);
 });
 
-
-test('normalizeURL strip trailing slash', () => {
-    const input = 'https://www.linkedin.com/feed/';
-    const actual = normalizeURL(input);
-    const expected = 'www.linkedin.com/feed';
-    expect(actual).toEqual(expected);
+test("normalizeURL strip trailing slash", () => {
+  const input = "https://www.linkedin.com/feed/";
+  const actual = normalizeURL(input);
+  const expected = "www.linkedin.com/feed";
+  expect(actual).toEqual(expected);
 });
 
-test('normalizeURL strip capitals', () => {
-    const input = 'https://www.LINKEDIN.com/feed/';
-    const actual = normalizeURL(input);
-    const expected = 'www.linkedin.com/feed';
-    expect(actual).toEqual(expected);
+test("normalizeURL strip capitals", () => {
+  const input = "https://www.LINKEDIN.com/feed/";
+  const actual = normalizeURL(input);
+  const expected = "www.linkedin.com/feed";
+  expect(actual).toEqual(expected);
 });
 
-test('normalizeURL strip http', () => {
-    const input = 'http://www.linkedin.com/feed/';
-    const actual = normalizeURL(input);
-    const expected = 'www.linkedin.com/feed';
-    expect(actual).toEqual(expected);
+test("normalizeURL strip http", () => {
+  const input = "http://www.linkedin.com/feed/";
+  const actual = normalizeURL(input);
+  const expected = "www.linkedin.com/feed";
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML absolute", () => {
+  const inputHtmlFile = `
+        <html>
+            <body>
+                <a href='https://www.linkedin.com/feed/'>
+                    Feeds | LinkedIn
+                </a>
+            </body>
+        </html>
+    `;
+  const inputBaseUrl = "https://www.linkedin.com";
+  const actual = getURLsFromHTML(inputHtmlFile, inputBaseUrl);
+  const expected = ["https://www.linkedin.com/feed/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", () => {
+  const inputHtmlFile = `
+        <html>
+            <body>
+                <a href='/feed/'>
+                    Feeds | LinkedIn
+                </a>
+            </body>
+        </html>
+    `;
+  const inputBaseUrl = "https://www.linkedin.com";
+  const actual = getURLsFromHTML(inputHtmlFile, inputBaseUrl);
+  const expected = ["https://www.linkedin.com/feed/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML both", () => {
+  const inputHtmlFile = `
+        <html>
+            <body>
+                <a href='https://www.linkedin.com/feed/'>
+                    Feeds | LinkedIn
+                </a>
+                <a href='/mynetwork/'>
+                    LinkedIn
+                </a>
+            </body>
+        </html>
+    `;
+  const inputBaseUrl = "https://www.linkedin.com";
+  const actual = getURLsFromHTML(inputHtmlFile, inputBaseUrl);
+  const expected = [
+    "https://www.linkedin.com/feed/",
+    "https://www.linkedin.com/mynetwork/",
+  ];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML invalid url", () => {
+  const inputHtmlFile = `
+        <html>
+            <body>
+                <a href='invalid'>
+                    Invalid URL
+                </a>
+            </body>
+        </html>
+    `;
+  const inputBaseUrl = "https://www.linkedin.com";
+  const actual = getURLsFromHTML(inputHtmlFile, inputBaseUrl);
+  const expected = [];
+  expect(actual).toEqual(expected);
 });
